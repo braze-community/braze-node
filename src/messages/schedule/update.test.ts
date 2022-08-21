52b67bb62120235ba2ce30db6f892e047f6b6199
Mark <mark@remarkablemark.org>
2022-08-20 20:36:17 -0400

    feat: add messages schedule update
    https://www.braze.com/docs/api/endpoints/messaging/schedule_messages/post_update_scheduled_messages/

---
+++ src/messages/schedule/update.test.ts
@@ -0,0 +1,52 @@
+import { post } from '../../common/request'
+import { update } from '.'
+import type { MessagesScheduleUpdateObject } from './types'
+
+jest.mock('../../common/request')
+const mockedPost = jest.mocked(post)
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('/messages/schedule/update', () => {
+  const apiUrl = 'https://rest.iad-01.braze.com'
+  const apiKey = 'apiKey'
+
+  const body: MessagesScheduleUpdateObject = {
+    schedule_id: 'schedule_identifier',
+    schedule: {
+      time: '2017-05-24T20:30:36Z',
+    },
+    messages: {
+      apple_push: {
+        alert: 'Updated Message!',
+        badge: 1,
+      },
+      android_push: {
+        title: 'Updated title!',
+        alert: 'Updated message!',
+      },
+      sms: {
+        subscription_group_id: 'subscription_group_identifier',
+        message_variation_id: 'message_variation_identifier',
+        body: 'This is my SMS body.',
+        app_id: 'app_identifier',
+      },
+    },
+  }
+
+  const data = {}
+
+  it('calls request with url and body', async () => {
+    mockedPost.mockResolvedValueOnce(data)
+    expect(await update(apiUrl, apiKey, body)).toBe(data)
+    expect(mockedPost).toBeCalledWith(`${apiUrl}/messages/schedule/update`, body, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    })
+    expect(mockedPost).toBeCalledTimes(1)
+  })
+})
