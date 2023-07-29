bdd0279cd8befc8dfa3fae6927b536015d318d59
Mark <mark@remarkablemark.org>
2023-07-29 17:07:43 -0400

    refactor(templates): rename directories

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,2 +1,2 @@
-export * as content_blocks from './content-blocks'
-export * as email_templates from './email-templates'
+export * as content_blocks from './content_blocks'
+export * as email_templates from './email_templates'
