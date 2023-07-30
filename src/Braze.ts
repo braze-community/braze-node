d900ec0f0e4fba2e476584a9f62a8f411ec83ce0
Mark <mark@remarkablemark.org>
2023-07-30 00:09:12 -0400

    docs: refactor Braze and add TSDoc

--- src/Braze.ts
+++ src/Braze.ts
@@ -14,12 +14,12 @@ export class Braze {
   /**
    * {@link https://www.braze.com/docs/api/basics/#endpoints}
    */
-  private apiUrl: string
+  private readonly apiUrl: string
 
   /**
-   * {@link https://www.braze.com/docs/api/basics/#app-group-rest-api-keys}
+   * {@link https://www.braze.com/docs/user_guide/administrative/app_settings/api_settings_tab/}
    */
-  private apiKey: string
+  private readonly apiKey: string
 
   /**
    * @param apiUrl - Braze REST endpoint.
@@ -38,179 +38,385 @@ export class Braze {
     this.apiKey = apiKey
   }
 
+  /* eslint-disable tsdoc/syntax */
   campaigns = {
     trigger: {
       schedule: {
-        create: (body: campaigns.trigger.schedule.CampaignsTriggerScheduleCreateObject) =>
-          campaigns.trigger.schedule.create(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /campaigns/trigger/schedule/create
+         */
+        create: (body: campaigns.trigger.schedule.CampaignsTriggerScheduleCreateObject) => {
+          return campaigns.trigger.schedule.create(this.apiUrl, this.apiKey, body)
+        },
 
-        delete: (body: campaigns.trigger.schedule.CampaignsTriggerScheduleDeleteObject) =>
-          campaigns.trigger.schedule._delete(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /campaigns/trigger/schedule/delete
+         */
+        delete: (body: campaigns.trigger.schedule.CampaignsTriggerScheduleDeleteObject) => {
+          return campaigns.trigger.schedule._delete(this.apiUrl, this.apiKey, body)
+        },
 
-        update: (body: campaigns.trigger.schedule.CampaignsTriggerScheduleUpdateObject) =>
-          campaigns.trigger.schedule.update(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /campaigns/trigger/schedule/update
+         */
+        update: (body: campaigns.trigger.schedule.CampaignsTriggerScheduleUpdateObject) => {
+          return campaigns.trigger.schedule.update(this.apiUrl, this.apiKey, body)
+        },
       },
 
-      send: (body: campaigns.trigger.CampaignsTriggerSendObject) =>
-        campaigns.trigger.send(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /campaigns/trigger/send
+       */
+      send: (body: campaigns.trigger.CampaignsTriggerSendObject) => {
+        return campaigns.trigger.send(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
   canvas = {
     trigger: {
       schedule: {
-        create: (body: canvas.trigger.schedule.CanvasTriggerScheduleCreateObject) =>
-          canvas.trigger.schedule.create(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /canvas/trigger/schedule/create
+         */
+        create: (body: canvas.trigger.schedule.CanvasTriggerScheduleCreateObject) => {
+          return canvas.trigger.schedule.create(this.apiUrl, this.apiKey, body)
+        },
 
-        delete: (body: canvas.trigger.schedule.CanvasTriggerScheduleDeleteObject) =>
-          canvas.trigger.schedule._delete(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /canvas/trigger/schedule/delete
+         */
+        delete: (body: canvas.trigger.schedule.CanvasTriggerScheduleDeleteObject) => {
+          return canvas.trigger.schedule._delete(this.apiUrl, this.apiKey, body)
+        },
 
-        update: (body: canvas.trigger.schedule.CanvasTriggerScheduleUpdateObject) =>
-          canvas.trigger.schedule.update(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /canvas/trigger/schedule/update
+         */
+        update: (body: canvas.trigger.schedule.CanvasTriggerScheduleUpdateObject) => {
+          return canvas.trigger.schedule.update(this.apiUrl, this.apiKey, body)
+        },
       },
 
-      send: (body: canvas.trigger.CanvasTriggerSendObject) =>
-        canvas.trigger.send(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /canvas/trigger/send
+       */
+      send: (body: canvas.trigger.CanvasTriggerSendObject) => {
+        return canvas.trigger.send(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
+  /**
+   * {@link https://www.braze.com/docs/api/endpoints/catalogs}
+   */
   catalogs = {
     synchronous: {
-      list: () => catalogs.synchronous.listCatalogs(this.apiUrl, this.apiKey),
+      /**
+       * GET /catalogs
+       */
+      list: () => {
+        return catalogs.synchronous.listCatalogs(this.apiUrl, this.apiKey)
+      },
+
+      /**
+       * GET /catalogs/{catalog_name}/items
+       */
       items: <T extends catalogs.synchronous.CatalogListItem>(
         body: catalogs.synchronous.CatalogListItemsBody,
-      ) => catalogs.synchronous.getListCatalogItemsIterator<T>(this.apiUrl, this.apiKey, body),
+      ) => {
+        return catalogs.synchronous.getListCatalogItemsIterator<T>(this.apiUrl, this.apiKey, body)
+      },
+
+      /**
+       * GET /catalogs/{catalog_name}/items/{item_id}
+       */
       item: <T extends catalogs.synchronous.CatalogListItem>(
         body: catalogs.synchronous.CatalogListItemBody,
-      ) => catalogs.synchronous.getCatalogItem<T>(this.apiUrl, this.apiKey, body),
+      ) => {
+        return catalogs.synchronous.getCatalogItem<T>(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
+  /**
+   * {@link https://www.braze.com/docs/api/endpoints/email}
+   */
   email = {
-    blacklist: (body: email.EmailBlacklistObject) =>
-      email.blacklist(this.apiUrl, this.apiKey, body),
-
-    bounce: {
-      remove: (body: email.bounce.EmailBounceRemoveObject) =>
-        email.bounce.remove(this.apiUrl, this.apiKey, body),
+    /**
+     * POST /email/blacklist
+     */
+    blacklist: (body: email.EmailBlacklistObject) => {
+      return email.blacklist(this.apiUrl, this.apiKey, body)
     },
 
+    /**
+     * POST /email/blacklist
+     */
+    bounce: {
+      remove: (body: email.bounce.EmailBounceRemoveObject) => {
+        return email.bounce.remove(this.apiUrl, this.apiKey, body)
+      },
+    },
+
+    /**
+     * POST /email/spam/remove
+     */
     spam: {
-      remove: (body: email.spam.EmailSpamRemoveObject) =>
-        email.spam.remove(this.apiUrl, this.apiKey, body),
+      remove: (body: email.spam.EmailSpamRemoveObject) => {
+        return email.spam.remove(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
+  /**
+   * {@link https://www.braze.com/docs/api/endpoints/messaging}
+   */
   messages = {
     schedule: {
-      create: (body: messages.schedule.MessagesScheduleCreateObject) =>
-        messages.schedule.create(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /messages/schedule/create
+       */
+      create: (body: messages.schedule.MessagesScheduleCreateObject) => {
+        return messages.schedule.create(this.apiUrl, this.apiKey, body)
+      },
 
-      delete: (body: messages.schedule.MessagesScheduleDeleteObject) =>
-        messages.schedule._delete(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /messages/schedule/delete
+       */
+      delete: (body: messages.schedule.MessagesScheduleDeleteObject) => {
+        return messages.schedule._delete(this.apiUrl, this.apiKey, body)
+      },
 
-      update: (body: messages.schedule.MessagesScheduleUpdateObject) =>
-        messages.schedule.update(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /messages/schedule/update
+       */
+      update: (body: messages.schedule.MessagesScheduleUpdateObject) => {
+        return messages.schedule.update(this.apiUrl, this.apiKey, body)
+      },
     },
 
-    scheduled_broadcasts: (body: Parameters<typeof messages.scheduled_broadcasts>[2]) =>
-      messages.scheduled_broadcasts(this.apiUrl, this.apiKey, body),
+    /**
+     * GET /messages/scheduled_broadcasts
+     */
+    scheduled_broadcasts: (body: Parameters<typeof messages.scheduled_broadcasts>[2]) => {
+      return messages.scheduled_broadcasts(this.apiUrl, this.apiKey, body)
+    },
 
-    send: (body: messages.MessagesSendObject) => messages.send(this.apiUrl, this.apiKey, body),
+    /**
+     * POST /messages/send
+     */
+    send: (body: messages.MessagesSendObject) => {
+      return messages.send(this.apiUrl, this.apiKey, body)
+    },
   }
 
   sends = {
     id: {
-      create: (body: sends.id.SendsIdCreateObject) =>
-        sends.id.create(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /sends/id/create
+       */
+      create: (body: sends.id.SendsIdCreateObject) => {
+        return sends.id.create(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
+  /**
+   * {@link https://www.braze.com/docs/api/endpoints/subscription_groups}
+   */
   subscription = {
     status: {
-      get: (body: subscription.status.SubscriptionStatusGetObject) =>
-        subscription.status.get(this.apiUrl, this.apiKey, body),
+      /**
+       * GET /subscription/status/get
+       */
+      get: (body: subscription.status.SubscriptionStatusGetObject) => {
+        return subscription.status.get(this.apiUrl, this.apiKey, body)
+      },
 
-      set: (body: subscription.status.SubscriptionStatusSetObject) =>
-        subscription.status.set(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /subscription/status/set
+       */
+      set: (body: subscription.status.SubscriptionStatusSetObject) => {
+        return subscription.status.set(this.apiUrl, this.apiKey, body)
+      },
     },
 
     user: {
-      status: (body: subscription.user.SubscriptionUserStatusObject) =>
-        subscription.user.status(this.apiUrl, this.apiKey, body),
+      /**
+       * GET /subscription/user/status
+       */
+      status: (body: subscription.user.SubscriptionUserStatusObject) => {
+        return subscription.user.status(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
+  /**
+   * {@link https://www.braze.com/docs/api/endpoints/templates}
+   */
   templates = {
     content_blocks: {
-      get: (body: templates.content_blocks.ContentBlockBody) =>
-        templates.content_blocks.getContentBlock(this.apiUrl, this.apiKey, body),
-      list: (body: templates.content_blocks.ContentBlockListBody) =>
-        templates.content_blocks.listContentBlocks(this.apiUrl, this.apiKey, body),
+      /**
+       * GET /content_blocks/info
+       */
+      get: (body: templates.content_blocks.ContentBlockBody) => {
+        return templates.content_blocks.getContentBlock(this.apiUrl, this.apiKey, body)
+      },
+
+      /**
+       * GET /content_blocks/list
+       */
+      list: (body: templates.content_blocks.ContentBlockListBody) => {
+        return templates.content_blocks.listContentBlocks(this.apiUrl, this.apiKey, body)
+      },
     },
+
     email_templates: {
-      get: (body: templates.email_templates.EmailTemplateBody) =>
-        templates.email_templates.getEmailTemplate(this.apiUrl, this.apiKey, body),
-      list: (body: templates.email_templates.EmailTemplateListBody) =>
-        templates.email_templates.getEmailTemplateList(this.apiUrl, this.apiKey, body),
-      create: (body: templates.email_templates.CreateEmailTemplateBody) =>
-        templates.email_templates.createEmailTemplate(this.apiUrl, this.apiKey, body),
-      update: (body: templates.email_templates.UpdateEmailTemplateBody) =>
-        templates.email_templates.updateEmailTemplate(this.apiUrl, this.apiKey, body),
+      /**
+       * GET /templates/email/info
+       */
+      get: (body: templates.email_templates.EmailTemplateBody) => {
+        return templates.email_templates.getEmailTemplate(this.apiUrl, this.apiKey, body)
+      },
+
+      /**
+       * GET /templates/email/list
+       */
+      list: (body: templates.email_templates.EmailTemplateListBody) => {
+        return templates.email_templates.getEmailTemplateList(this.apiUrl, this.apiKey, body)
+      },
+
+      /**
+       * POST /templates/email/create
+       */
+      create: (body: templates.email_templates.CreateEmailTemplateBody) => {
+        return templates.email_templates.createEmailTemplate(this.apiUrl, this.apiKey, body)
+      },
+
+      /**
+       * POST /templates/email/update
+       */
+      update: (body: templates.email_templates.UpdateEmailTemplateBody) => {
+        return templates.email_templates.updateEmailTemplate(this.apiUrl, this.apiKey, body)
+      },
     },
   }
 
   transactional = {
     v1: {
       campaigns: {
+        /**
+         * POST /transactional/v1/campaigns/{campaign_id}/send
+         */
         send: (
           campaignId: string,
           body: transactional.v1.campaigns.TransactionalV1CampaignsSendObject,
-        ) => transactional.v1.campaigns.send(this.apiUrl, this.apiKey, campaignId, body),
+        ) => {
+          return transactional.v1.campaigns.send(this.apiUrl, this.apiKey, campaignId, body)
+        },
       },
     },
   }
 
+  /**
+   * {@link https://www.braze.com/docs/api/endpoints/user_data}
+   */
   users = {
     alias: {
-      new: (body: users.alias.UsersAliasObject) => users.alias._new(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /users/alias/new
+       */
+      new: (body: users.alias.UsersAliasObject) => {
+        return users.alias._new(this.apiUrl, this.apiKey, body)
+      },
     },
 
-    delete: (body: users.UsersDeleteObject) => users._delete(this.apiUrl, this.apiKey, body),
+    /**
+     * POST /users/delete
+     */
+    delete: (body: users.UsersDeleteObject) => {
+      return users._delete(this.apiUrl, this.apiKey, body)
+    },
 
     export: {
-      global_control_group: (body: users.export.UsersExportGlobalControlGroupObject) =>
-        users.export.global_control_group(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /users/export/global_control_group
+       */
+      global_control_group: (body: users.export.UsersExportGlobalControlGroupObject) => {
+        return users.export.global_control_group(this.apiUrl, this.apiKey, body)
+      },
 
-      ids: (body: users.export.UsersExportIdsObject) =>
-        users.export.ids(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /users/export/ids
+       */
+      ids: (body: users.export.UsersExportIdsObject) => {
+        return users.export.ids(this.apiUrl, this.apiKey, body)
+      },
 
-      segment: (body: users.export.UsersExportSegmentObject) =>
-        users.export.segment(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /users/export/segment
+       */
+      segment: (body: users.export.UsersExportSegmentObject) => {
+        return users.export.segment(this.apiUrl, this.apiKey, body)
+      },
     },
 
+    /**
+     * {@link https://www.braze.com/docs/api/endpoints/user_data/external_id_migration}
+     */
     external_ids: {
-      remove: (body: users.external_ids.UsersExternalIdsRemoveObject) =>
-        users.external_ids.remove(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /users/external_ids/remove
+       */
+      remove: (body: users.external_ids.UsersExternalIdsRemoveObject) => {
+        return users.external_ids.remove(this.apiUrl, this.apiKey, body)
+      },
 
-      rename: (body: users.external_ids.UsersExternalIdsRenameObject) =>
-        users.external_ids.rename(this.apiUrl, this.apiKey, body),
+      /**
+       * POST /users/external_ids/rename
+       */
+      rename: (body: users.external_ids.UsersExternalIdsRenameObject) => {
+        return users.external_ids.rename(this.apiUrl, this.apiKey, body)
+      },
     },
 
-    identify: (body: users.UsersIdentifyObject) => users.identify(this.apiUrl, this.apiKey, body),
+    /**
+     * POST /users/identify
+     */
+    identify: (body: users.UsersIdentifyObject) => {
+      return users.identify(this.apiUrl, this.apiKey, body)
+    },
 
-    merge: (body: users.UsersMergeObject) => users.merge(this.apiUrl, this.apiKey, body),
+    /**
+     * POST /users/merge
+     */
+    merge: (body: users.UsersMergeObject) => {
+      return users.merge(this.apiUrl, this.apiKey, body)
+    },
 
-    track: (body: users.UsersTrackObject, bulk?: boolean) =>
-      users.track(this.apiUrl, this.apiKey, body, bulk),
+    /**
+     * POST /users/track
+     */
+    track: (body: users.UsersTrackObject, bulk?: boolean) => {
+      return users.track(this.apiUrl, this.apiKey, body, bulk)
+    },
   }
 
   v2 = {
+    /**
+     * {@link https://www.braze.com/docs/api/endpoints/subscription_groups}
+     */
     subscription: {
       status: {
-        set: (body: v2.subscription.status.V2SubscriptionStatusSetObject) =>
-          v2.subscription.status.set(this.apiUrl, this.apiKey, body),
+        /**
+         * POST /v2/subscription/status/set
+         */
+        set: (body: v2.subscription.status.V2SubscriptionStatusSetObject) => {
+          return v2.subscription.status.set(this.apiUrl, this.apiKey, body)
+        },
       },
     },
   }
+  /* eslint-enable tsdoc/syntax */
 }
