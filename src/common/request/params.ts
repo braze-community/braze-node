5a310d86f224a04e456f6dc290f249118f202b6f
Mark <mark@remarkablemark.org>
2023-07-30 13:12:19 -0400

    refactor(common): refactor params

--- src/common/request/params.ts
+++ src/common/request/params.ts
@@ -3,17 +3,25 @@ import { URLSearchParams } from 'url'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function buildParams(body: Record<string, any>): URLSearchParams {
   return Object.entries(body).reduce((params, [key, value]) => {
-    if (typeof value === 'boolean') {
-      params.append(key, JSON.stringify(value))
-    } else if (typeof value === 'string') {
-      params.append(key, value)
-    } else if (typeof value === 'number') {
-      params.append(key, value.toString())
-    } else if (typeof value === 'undefined') {
-      // Intentionally skip value
-    } else {
-      throw new Error(`Unhandled param type for "${key}"`)
+    if (value !== undefined) {
+      params.append(key, getValue(value, key))
     }
     return params
   }, new URLSearchParams())
+}
+
+function getValue(value: unknown, key: string) {
+  switch (typeof value) {
+    case 'boolean':
+      return JSON.stringify(value)
+
+    case 'string':
+      return value
+
+    case 'number':
+      return value.toString()
+
+    default:
+      throw new Error(`Unhandled param type for "${key}"`)
+  }
 }
