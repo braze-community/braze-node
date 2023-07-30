89bf6e10436ecca6609d067bf3cad3f89be58283
Mark <mark@remarkablemark.org>
2023-07-30 13:46:03 -0400

    refactor(common): update buildParams to handle array values

--- src/common/request/params.test.ts
+++ src/common/request/params.test.ts
@@ -1,27 +1,69 @@
 import { buildParams } from './params'
 
-describe('params', () => {
-  it('should handle string', () => {
-    const params = buildParams({ a: 'string' })
-    expect(params.toString()).toEqual('a=string')
+describe('buildParams', () => {
+  describe.each([undefined, null])('%p', (value) => {
+    it('handles single', () => {
+      const params = buildParams({ a: value })
+      expect(params.toString()).toEqual('')
+    })
+
+    it('handles multiple', () => {
+      const params = buildParams({ a: value, b: value })
+      expect(params.toString()).toEqual('')
+    })
   })
 
-  it('should handle number', () => {
-    const params = buildParams({ b: 1 })
-    expect(params.toString()).toEqual('b=1')
+  describe('string', () => {
+    it('handles single', () => {
+      const params = buildParams({ a: 'string' })
+      expect(params.toString()).toEqual('a=string')
+    })
+
+    it('handles multiple', () => {
+      const params = buildParams({ a: 'a', b: '', c: 'c' })
+      expect(params.toString()).toEqual('a=a&b=&c=c')
+    })
   })
 
-  it('should handle boolean', () => {
-    const params = buildParams({ a: true, b: false })
-    expect(params.toString()).toEqual('a=true&b=false')
+  describe('number', () => {
+    it('handles single', () => {
+      const params = buildParams({ b: 1 })
+      expect(params.toString()).toEqual('b=1')
+    })
+
+    it('handles multiple', () => {
+      const params = buildParams({ a: 0, b: 1 })
+      expect(params.toString()).toEqual('a=0&b=1')
+    })
   })
 
-  it('should handle undefined', () => {
-    const params = buildParams({ a: undefined })
-    expect(params.toString()).toEqual('')
+  describe('boolean', () => {
+    it('handles single', () => {
+      const params = buildParams({ b: false })
+      expect(params.toString()).toEqual('b=false')
+    })
+
+    it('handles multiple', () => {
+      const params = buildParams({ a: true, b: false })
+      expect(params.toString()).toEqual('a=true&b=false')
+    })
   })
 
-  it('should fail on anything else', () => {
-    expect(() => buildParams({ a: [1] })).toThrowError('Unhandled param type for "a"')
+  describe('array', () => {
+    it('handles single', () => {
+      const params = buildParams({ a: [1] })
+      expect(params.toString()).toEqual('a=1')
+    })
+
+    it('handles multiple', () => {
+      const params = buildParams({ a: [1, '2', true, undefined], b: ['foo', 'bar', null] })
+      expect(params.toString()).toEqual('a=1&a=2&a=true&b=foo&b=bar')
+    })
+  })
+
+  describe('error', () => {
+    it.each([new Date(), {}, Symbol('symbol'), () => {}])('throws when value is %p', (value) => {
+      expect(() => buildParams({ a: value })).toThrowError('Unhandled param type for key "a"')
+    })
   })
 })
--- src/common/request/params.ts
+++ src/common/request/params.ts
@@ -3,14 +3,27 @@ import { URLSearchParams } from 'url'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function buildParams(body: Record<string, any>): URLSearchParams {
   return Object.entries(body).reduce((params, [key, value]) => {
-    if (value !== undefined) {
-      params.append(key, getValue(value, key))
-    }
-    return params
+    return appendParams(params, key, value)
   }, new URLSearchParams())
 }
 
-function getValue(value: unknown, key: string) {
+function appendParams(params: URLSearchParams, key: string, value: unknown): URLSearchParams {
+  if (value === undefined || value === null) {
+    return params
+  }
+
+  if (Array.isArray(value)) {
+    value.forEach((currentValue) => {
+      appendParams(params, key, currentValue)
+    })
+    return params
+  }
+
+  params.append(key, getValue(key, value))
+  return params
+}
+
+function getValue(key: string, value: unknown): string {
   switch (typeof value) {
     case 'boolean':
       return JSON.stringify(value)
@@ -22,6 +35,6 @@ function getValue(value: unknown, key: string) {
       return value.toString()
 
     default:
-      throw new Error(`Unhandled param type for "${key}"`)
+      throw new Error(`Unhandled param type for key "${key}"`)
   }
 }
